67b32cef2fa059a28e7274203fad14c0683436ba
Alex Khadiwala <alexkhadiwala@gmail.com>
2023-06-26 12:48:31 -0700

    Fixing issues with seeding the database

--- seed_database.js
+++ seed_database.js
@@ -1,14 +1,25 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/gddy.sqlite');
 const fs = require('fs');
-const data = fs.readFileSync('./db/seed_database.sql', 'utf8');
+const sql = fs.readFileSync('./db/seed_database.sql', 'utf8');
 
-db.run(data, {}, (err) => {
-  if (err) {
-    return console.log(err.message);
-  }
-
+async function migrate() {
+  await db.exec(sql, (err) => {
+    if (err) {
+      console.log(err.message);
+      process.exit(1);
+    }
+  });
   console.log('Database seeded');
-});
+}
+
+migrate()
+  .catch((err) => {
+    console.log(err.message);
+  })
+  .finally(() => {
+    db.close();
+  });
+
